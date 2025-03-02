167eff12060622b5d2e9000b386417347697134d
Devansh <thedevanshvala@gmail.com>
2025-03-03 01:02:29 +0530

    1> Added Some Code to improve login page and added chat window to MainWindow

--- TSN_WPF/MainWindow.xaml.cs
+++ TSN_WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,34 @@ namespace TSN_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<ChatMessage> Messages { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            Messages = new ObservableCollection<ChatMessage>();
+            lstMessages.ItemsSource = Messages;
         }
+
+        private void SendMessage_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtMessage.Text))
+            {
+                Messages.Add(new ChatMessage
+                {
+                    Message = txtMessage.Text,
+                    Time = DateTime.Now.ToString("hh:mm tt") // Format time as "12:30 PM"
+                });
+
+                txtMessage.Clear();
+                lstMessages.ScrollIntoView(Messages[Messages.Count - 1]); // Auto-scroll
+            }
+        }
+    }
+
+    public class ChatMessage
+    {
+        public string Message { get; set; }
+        public string Time { get; set; }
     }
 }
